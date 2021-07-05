0324020341c92087877c7a757a9e88626ce27b10
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-07-04 19:53:53 -0700

    added companion to CareUser converted CareUser to class to allow for weak companion

--- CareForMe/Features/Auth/Model/CareUser.swift
+++ CareForMe/Features/Auth/Model/CareUser.swift
@@ -7,7 +7,16 @@
 
 import Foundation
 
-struct CareUser: Codable {
+class CareUser: Codable {
+    
     let userId: String
     let displayName: String
+    weak var companion: CareUser?
+    
+    init(userId: String, displayName: String, companion: CareUser? = nil) {
+        self.userId = userId
+        self.displayName = displayName
+        self.companion = companion
+    }
+    
 }
