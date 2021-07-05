5f80779d7537a57dd0a24de36fae0123496871e2
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-07-05 08:59:38 -0700

    implemented views on notification detail

--- CareForMe/Features/Notifications/Controller/View Controllers/NotificationDetailViewController.swift
+++ CareForMe/Features/Notifications/Controller/View Controllers/NotificationDetailViewController.swift
@@ -8,27 +8,33 @@
 import UIKit
 
 class NotificationDetailViewController: UIViewController {
+    // MARK: - Properties -
     var notification: CareNotification {
         didSet {
             displayNotification()
         }
     }
+    // MARK: - Views -
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
     
-    private lazy var stackView: UIStackView = {
+    private lazy var contentView: UIStackView = {
         let stack = UIStackView(arrangedSubviews: [titleLabel, textLabel])
         stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .vertical
         stack.alignment = .leading
-        stack.distribution = .fillEqually
+        stack.distribution = .fill
         stack.spacing = 10
-        
         return stack
     }()
     
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.numberOfLines = 1
-        label.font = .preferredFont(for: .title1, weight: .bold)
+        label.numberOfLines = 0
+        label.font = .preferredFont(for: .title2, weight: .bold)
         return label
     }()
     
@@ -38,7 +44,7 @@ class NotificationDetailViewController: UIViewController {
         label.font = .preferredFont(for: .body, weight: .regular)
         return label
     }()
-    
+    // MARK: - Init -
     init(notification: CareNotification) {
         self.notification = notification
         super.init(nibName: nil, bundle: nil)
@@ -48,28 +54,33 @@ class NotificationDetailViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("programmatic view")
     }
-    
+    // MARK: - View Lifecycle -
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
     }
     
     private func setupViews() {
-        if !view.subviews.contains(stackView) {
-            view.addSubview(stackView)
-            let view = UIView(frame: CGRect(x: 0, y: 0, width: stackView.frame.width, height: stackView.frame.height - titleLabel.frame.height - textLabel.frame.height))
-            stackView.addArrangedSubview(view)
+        displayNotification()
+        
+        if !view.subviews.contains(scrollView) {
+            scrollView.addSubview(contentView)
+            view.addSubview(scrollView)
             constraints()
         }
-        displayNotification()
     }
     
     private func constraints() {
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
-            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
-            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10)
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+        
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10)
         ])
     }
     
@@ -78,6 +89,5 @@ class NotificationDetailViewController: UIViewController {
         titleLabel.text = notification.categoryTitle
         textLabel.text = notification.text
     }
-    
     
 }
