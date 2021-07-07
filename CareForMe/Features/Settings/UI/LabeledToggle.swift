56955450758e209a68aaf6a346ea0aee6db6b723
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-07-07 16:00:37 -0700

    Initial Settings UI

---
+++ CareForMe/Features/Settings/UI/LabeledToggle.swift
@@ -0,0 +1,57 @@
+//
+//  LabeledToggle.swift
+//  CareForMe
+//
+//  Created by Kenneth Dubroff on 7/7/21.
+//
+
+import UIKit
+
+class LabeledToggleSwitch: UIView {
+    var title: String
+    
+    lazy var stackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [titleLabel, toggleSwitch])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.distribution = .equalSpacing
+        stack.alignment = .top
+        return stack
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let label  = UILabel()
+        label.text = title
+        label.font = .preferredFont(for: .body, weight: .bold)
+        return label
+    }()
+    
+    lazy var toggleSwitch: UISwitch = {
+        var toggleSwitch = UISwitch()
+        return toggleSwitch
+    }()
+    
+    init(title: String, toggleFunction selector: Selector, target: Any, event: UIControl.Event = .touchUpInside) {
+        self.title = title
+        super.init(frame: .zero)
+        toggleSwitch.addTarget(target, action: selector, for: event)
+        subviews()
+    }
+    
+    private func subviews() {
+        addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            stackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor)
+        ])
+    }
+    
+    func setSwitchState(on: Bool) {
+        toggleSwitch.isOn = on
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Programmatic View")
+    }
+}
