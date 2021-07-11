ff1c133a571663fe396fc74cec27c4a61cff0316
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-07-11 16:51:27 -0700

    - Created CodableColor struct using HSB initializer

--- CareForMe/Features/Needs/Model/NeedsModel.swift
+++ CareForMe/Features/Needs/Model/NeedsModel.swift
@@ -11,6 +11,7 @@ struct NeedsCategory: Codable, Equatable {
     var id: UUID = UUID()
     var title: String
     var needs: [Need] = []
+    var color: CodableColor = .init(uiColor: .systemBackground)
     
     @discardableResult mutating func addNeed(_ need: Need) -> Bool {
         guard !needs.contains(need) else { return false }
@@ -26,4 +27,16 @@ struct NeedsCategory: Codable, Equatable {
 struct Need: Codable, Equatable {
     var needPhotoModel: NamedPhoto
     lazy var title: String = needPhotoModel.title
+}
+
+struct CodableColor : Codable {
+    var hue : CGFloat = 0.0, saturation: CGFloat = 0.0, brightness: CGFloat = 0.0, alpha: CGFloat = 0.0
+
+    var uiColor : UIColor {
+        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: alpha)
+    }
+
+    init(uiColor : UIColor) {
+        uiColor.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
+    }
 }
