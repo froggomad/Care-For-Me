c2f442a582d69634fef42fc869402c0b1dfe2945
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-07-18 21:53:02 -0700

    Moved `TargetSelector` to model group

---
+++ CareForMe/Model/TargetSelector.swift
@@ -0,0 +1,13 @@
+//
+//  TargetSelector.swift
+//  CareForMe
+//
+//  Created by Kenneth Dubroff on 7/18/21.
+//
+
+import Foundation
+
+struct TargetSelector {
+    weak var target: AnyObject?
+    var selector: Selector
+}
