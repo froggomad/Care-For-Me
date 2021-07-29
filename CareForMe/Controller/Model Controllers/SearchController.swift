fa584ee5d29e5145d685ce76104f588d6e33756a
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-07-29 14:22:11 -0700

    refactored initialization pattern

--- CareForMe/Controller/Model Controllers/SearchController.swift
+++ CareForMe/Controller/Model Controllers/SearchController.swift
@@ -8,18 +8,22 @@
 import UIKit
 
 protocol SearchDelegate: UIViewController, UISearchControllerDelegate {
+    /// force unwrapped due to init pattern
+    var searchController: SearchController! { get set }
     func receiveSearchUpdate(_ results: [String])
 }
 
 class SearchController: UISearchController {
     
-    var searchSource: [String] = []
-    weak var searchDelegate: SearchDelegate?
+    var searchSource: [String]
+    weak var searchDelegate: SearchDelegate!
     
-    init(delegate: SearchDelegate) {
+    init(delegate: SearchDelegate, searchArray: [String]) {
+        self.searchSource = searchArray
         super.init(searchResultsController: delegate)
         self.searchDelegate = delegate
         self.delegate = delegate
+        delegate.searchController = self
         obscuresBackgroundDuringPresentation = false
         hidesNavigationBarDuringPresentation = false
     }
