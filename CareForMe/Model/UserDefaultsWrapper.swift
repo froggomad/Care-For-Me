300d164dbf653bb15fb690203269bae1a0e4aa37
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-08-05 22:04:06 -0700

    added UserDefaults wrapper

---
+++ CareForMe/Model/UserDefaultsWrapper.swift
@@ -0,0 +1,42 @@
+//
+//  UserDefaultsWrapper.swift
+//  CareForMe
+//
+//  Created by Kenneth Dubroff on 8/5/21.
+//
+
+import Foundation
+
+enum UserDefaultKey: String {
+    case hasOnboarded = "has_onboarded"
+    case lastLoggedInUser = "last_logged_in_user"
+}
+
+@propertyWrapper
+struct UserDefault<T> {
+    let key: UserDefaultKey
+    let defaultValue: T
+    
+    init(_ key: UserDefaultKey, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+    
+    var wrappedValue: T {
+        get {
+            return UserDefaults.standard.object(forKey: key.rawValue) as? T ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key.rawValue)
+        }
+    }
+}
+
+struct UserDefaultsConfig {
+    
+    @UserDefault(.hasOnboarded, defaultValue: false)
+    static var hasSeenAppIntroduction: Bool
+    
+    @UserDefault(.lastLoggedInUser, defaultValue: "")
+    static var lastLoggedInUsername: String
+}
