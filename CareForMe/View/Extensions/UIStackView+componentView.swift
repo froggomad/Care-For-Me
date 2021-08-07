0e8da228f9201e5e9066d77e23b48c04e597bcec
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-08-06 17:17:01 -0700

    added spacing parameter

--- CareForMe/View/Extensions/UIStackView+componentView.swift
+++ CareForMe/View/Extensions/UIStackView+componentView.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 extension UIStackView {
-    static func componentStack(elements: [UIView], distribution: UIStackView.Distribution = .fillProportionally, verticalAlignment: UIStackView.Alignment = .fill, horizontalAlignment: UIStackView.Alignment = .top) -> UIStackView {
+    static func componentStack(elements: [UIView], distribution: UIStackView.Distribution = .fillProportionally, verticalAlignment: UIStackView.Alignment = .fill, horizontalAlignment: UIStackView.Alignment = .top, spacing: CGFloat = 2) -> UIStackView {
         let stack = UIStackView(arrangedSubviews: elements)
 //        stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .vertical
         stack.distribution = distribution
         stack.alignment = verticalAlignment
-        stack.spacing = 2
+        stack.spacing = spacing
         
         let horizontalStack = UIStackView(arrangedSubviews: [stack])
         horizontalStack.alignment = horizontalAlignment
