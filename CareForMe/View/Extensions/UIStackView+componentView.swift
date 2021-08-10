fce09f52e6a4f85a63b141a40499e03865b5ac2c
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-08-09 17:53:42 -0700

    removed unused code

--- CareForMe/View/Extensions/UIStackView+componentView.swift
+++ CareForMe/View/Extensions/UIStackView+componentView.swift
@@ -10,7 +10,6 @@ import UIKit
 extension UIStackView {
     static func componentStack(elements: [UIView], distribution: UIStackView.Distribution = .fillProportionally, verticalAlignment: UIStackView.Alignment = .fill, horizontalAlignment: UIStackView.Alignment = .top, spacing: CGFloat = 2) -> UIStackView {
         let stack = UIStackView(arrangedSubviews: elements)
-//        stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .vertical
         stack.distribution = distribution
         stack.alignment = verticalAlignment
