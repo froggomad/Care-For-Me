c776a9cd5c6aa88879448aca30132aa7d3cf1a40
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-08-11 17:11:28 -0700

    added `additionalViews` to init

--- CareForMe/Features/Connect Users/Parchment/ParchmentPageViewController.swift
+++ CareForMe/Features/Connect Users/Parchment/ParchmentPageViewController.swift
@@ -23,13 +23,16 @@ class OnboardingViewController: InstructionViewController {
     let indicatorTitle: String
     let id: Int
     
-    init(id: Int, indicatorText: String, title: String, instructions: String, image: Gif?, caption: String? = nil, buttonTitle: String, selectionDelegate: TargetSelector) {
+    init(id: Int, indicatorText: String, title: String, instructions: String, image: Gif?, caption: String? = nil, buttonTitle: String, selectionDelegate: TargetSelector, additionalViews: [UIView]) {
         
         self.indicatorTitle = indicatorText
         self.id = id
         super.init(title: title, instructions: instructions, image: image, buttonTitle: buttonTitle, selectionDelegate: selectionDelegate)
-        
         instructionView.button.tag = id
+
+        for view in additionalViews {
+            instructionView.addView(view)
+        }
     }
     
     required init?(coder: NSCoder) {
