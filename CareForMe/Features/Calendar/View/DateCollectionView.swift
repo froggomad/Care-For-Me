8c632cce39093d9e71280b8408d41cfdc9802b6d
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-08-12 21:16:16 -0700

    reorganized file, added identifier to cell

--- CareForMe/Features/Calendar/View/DateCollectionView.swift
+++ CareForMe/Features/Calendar/View/DateCollectionView.swift
@@ -7,54 +7,46 @@
 
 import UIKit
 
-class DateCollectionView: UIView {
+class DateCollectionViewCell: UICollectionViewCell {
+    static let reuseIdentifier = String(describing: self)
     
-    private class DateCollectionViewCell: UICollectionViewCell {
-        
-        required init() {
-            super.init(frame: .zero)
-            setupViews()
-        }
-        
-        required init?(coder aDecoder: NSCoder) {
-            fatalError("programmatic view")
-        }
-        
-        private func setupViews() {
-            backgroundColor = .clear
-            layer.cornerRadius = 5
-            layer.masksToBounds = true
-            
-            addSubview(label)
-            NSLayoutConstraint.activate([
-                label.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
-                label.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
-                label.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
-                label.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
-            ])
-        }
-        
-        lazy var label: UILabel = {
-            let label = UILabel()
-            label.text = "00"
-            label.textAlignment = .center
-            label.font = UIFont.systemFont(ofSize: 16)
-            label.textColor = .systemGray
-            label.translatesAutoresizingMaskIntoConstraints=false
-            return label
-        }()
-        
-    }
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "00"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .systemGray
+        label.translatesAutoresizingMaskIntoConstraints=false
+        return label
+    }()
     
-    required init(delegate: UICollectionViewDelegate, dataSource: UICollectionViewDataSource) {
+    required init() {
         super.init(frame: .zero)
-        self.delegate = delegate
-        self.datasource = dataSource
+        setupViews()
     }
     
-    required init?(coder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         fatalError("programmatic view")
     }
+    
+    private func setupViews() {
+        backgroundColor = .clear
+        layer.cornerRadius = 5
+        layer.masksToBounds = true
+        
+        addSubview(label)
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            label.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            label.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    
+}
+
+class DateCollectionView: UIView {
     
     private weak var delegate: UICollectionViewDelegate?
     private weak var datasource: UICollectionViewDataSource?
@@ -64,7 +56,7 @@ class DateCollectionView: UIView {
         layout.sectionInset = .zero
         
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        cv.register(DateCollectionViewCell.self, forCellWithReuseIdentifier: "DateCollectionViewCell")
+        cv.register(DateCollectionViewCell.self, forCellWithReuseIdentifier: DateCollectionViewCell.reuseIdentifier)
         cv.delegate = delegate
         cv.dataSource = datasource
         cv.translatesAutoresizingMaskIntoConstraints = false
@@ -72,5 +64,14 @@ class DateCollectionView: UIView {
         return cv
     }()
 
+    required init(delegate: UICollectionViewDelegate, dataSource: UICollectionViewDataSource) {
+        super.init(frame: .zero)
+        self.delegate = delegate
+        self.datasource = dataSource
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("programmatic view")
+    }
     
 }
