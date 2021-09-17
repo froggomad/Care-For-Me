b24d1fd97e97c86536afba667d8955d596bb7192
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-09-17 06:28:27 -0700

    committing missed files for IQKeyboardManager dependency, activityVC

--- CareForMe/Features/Auth/Controller/View Controllers/LoginVC.swift
+++ CareForMe/Features/Auth/Controller/View Controllers/LoginVC.swift
@@ -13,6 +13,8 @@ protocol LoginProcessable: AnyObject {
 
 class LoginViewController: UIViewController, AuthControllable {
     
+    var activityViewController: ActivityIndicatorViewController?
+    
     private let authService = AuthService.shared
     lazy var authDelegate = AuthDelegate(self)
     
@@ -42,9 +44,17 @@ class LoginViewController: UIViewController, AuthControllable {
 extension LoginViewController: LoginProcessable {
     
     func processLogin(email: String, password: String) {
+        
+        activityViewController = createActivityView()
+        
         guard let emailAddress = loginView.emailAddress,
-              let password = loginView.password else { return }
-        authService.loginWithEmail(emailAddress, password: password) { result in
+              let password = loginView.password else {
+            removeActivityView(child: activityViewController!)
+            return
+        }
+        authService.loginWithEmail(emailAddress, password: password) { [weak self] result in
+            guard let self = self else { return }
+            self.removeActivityView(child: self.activityViewController!)
             switch result {
             case .success:
                 self.presentTabBar()
