c43daac4b2e3a968b53caa6cba77eb2dc82ba53b
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-09-19 11:02:02 -0700

    formatting

--- CareForMe/Features/Connect Users/Controller/OnboardingViewController.swift
+++ CareForMe/Features/Connect Users/Controller/OnboardingViewController.swift
@@ -13,7 +13,6 @@ class OnboardingViewController: InstructionViewController {
     let id: Int
     
     init(id: Int, indicatorText: String, title: String, instructions: String, image: Gif?, caption: String? = nil, buttonTitle: String, selectionDelegate: TargetSelector, additionalViews: [UIView]) {
-        
         self.indicatorTitle = indicatorText
         self.id = id
         super.init(title: title, instructions: instructions, image: image, buttonTitle: buttonTitle, selectionDelegate: selectionDelegate)
