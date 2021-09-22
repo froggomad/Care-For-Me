991d051429325c2845226cedda5b4759c399faad
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-09-22 13:37:28 -0700

    added initializer to provide default expiration date for `joinCode`

--- CareForMe/Features/Connect Users/Model/UserLink.swift
+++ CareForMe/Features/Connect Users/Model/UserLink.swift
@@ -10,7 +10,27 @@ import Foundation
 struct UserLink: Codable {
     let caregiver: CaregiverUser
     let client: ClientUser
-    var joinCode: String = Int.randomString()
+    var joinCode: String?
+    var expiresOn: Date
+    
+    init(caregiver: CaregiverUser, client: ClientUser, joinCode: String? = nil, expiresOn: Date = Date().adding(days: 7) ?? Date()) {
+        self.caregiver = caregiver
+        self.client = client
+        if joinCode == nil {
+            self.joinCode = Int.randomString()
+            // TODO: save joinCode
+        } else {
+            self.joinCode = joinCode
+        }
+        self.expiresOn = expiresOn
+    }
+    
+    init(caregiver: CaregiverUser, client: ClientUser, joinCode: String, expiresOn: Date) {
+        self.caregiver = caregiver
+        self.client = client
+        self.joinCode = joinCode
+        self.expiresOn = expiresOn
+    }
 }
 
 struct UserLinkController {
@@ -18,5 +38,11 @@ struct UserLinkController {
     
     static func sendLinkToDatabase(link: UserLink) {
         dbController.setUserValue(for: .userLink, with: link)
+    }
+}
+
+extension Date {
+    func adding(days: Int) -> Date? {
+        return Calendar.current.date(byAdding: .day, value: days, to: self)
     }
 }
