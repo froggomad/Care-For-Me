6072346a42f1ce86a4302f3df992b8cacda1194f
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-10-02 16:37:49 -0700

    added `linkRequest` Firebase Cloud Function

--- CareForMe/Core/Firestore Functions/FirestoreFunction.swift
+++ CareForMe/Core/Firestore Functions/FirestoreFunction.swift
@@ -8,13 +8,16 @@ import FirebaseFunctions
 
 enum FirestoreFunction {
     
-    case generateJoinCode(userId: String, joincode: String, userType: String)
+    case generateJoinCode(userId: String, joinCode: String, userType: String)
+    case linkRequest(userId: String, joinCode: String)
     
     /// The name of the Firebase Function (must equate to the name in Firebase)
     private var name: String {
         switch self {
         case .generateJoinCode:
             return "generateCode"
+        case .linkRequest:
+            return "linkRequest"
         }
     }
     /// - Used to send data to the Firebase Function
@@ -23,12 +26,43 @@ enum FirestoreFunction {
         switch self {
         case .generateJoinCode(let userId, let joinCode, let userType):
             return ["userId": userId, "joinCode": joinCode, "userType": userType]
+        case .linkRequest(let userId, let joinCode):
+            return ["userId": userId, "joinCode": joinCode]
         }
     }
     
-    func call(completion: @escaping (HTTPSCallableResult?, Error?) -> Void)  {
+    private func call(completion: @escaping (HTTPSCallableResult?, Error?) -> Void)  {
         let function = self
         Functions.functions().httpsCallable(function.name)
             .call(function.parameters, completion: completion)
+    }
+    
+    func callGenerateJoinCode(completion: @escaping (Result<UserLink, Error>) -> Void) {
+        call { result, error in
+        if let error = error {
+            completion(.failure(error))
+            return
+        }
+        let data = result?.data as? [String: String] ?? [:]
+        let careGiverId = data["caregiver"]
+        let clientId = data["client"]
+        let expirationDateString = data["expiresOn"] ?? ""
+        let joinCode = data["code"]
+        
+        let userLink = UserLink(caregiverId: careGiverId, clientId: clientId, joinCode: joinCode, expiresOn: DateFormatter.firebaseStringToDate(from: expirationDateString))
+        completion(.success(userLink))
+        }
+    }
+    
+    func callLinkRequest(completion: @escaping (Result<Bool, Error>) -> Void) {
+        call { result, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            let data = result?.data as? Bool
+            completion(.success(data ?? false))
+        }
+        
     }
 }
