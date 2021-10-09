8b74f93130be4b3882bcb04c5490ba9580d9cd4e
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-10-09 10:10:57 -0700

    added `JoinRequest` model

--- CareForMe/Features/Connect Users/Model/UserLink.swift
+++ CareForMe/Features/Connect Users/Model/UserLink.swift
@@ -7,6 +7,19 @@
 
 import Foundation
 
+struct JoinRequest: Codable {
+    let userId: String
+    let userType: UserType
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        userId = try container.decode(String.self, forKey: .userId)
+        let userTypeString = try container.decode(String.self, forKey: .userId)
+        let userType = UserType(rawValue: userTypeString) ?? .client
+        self.userType = userType
+    }
+}
+
 struct UserLink: Codable {
     let caregiverId: String?
     let clientId: String?
