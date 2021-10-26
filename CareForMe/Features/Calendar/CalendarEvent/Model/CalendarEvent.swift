7d502177b93ee9a412a17225962093f8d470af27
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-10-25 21:16:33 -0700

    added id property

--- CareForMe/Features/Calendar/CalendarEvent/Model/CalendarEvent.swift
+++ CareForMe/Features/Calendar/CalendarEvent/Model/CalendarEvent.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 
-struct CalendarEvent {
+struct CalendarEvent: Codable, Equatable {
+    let id: UUID
     let title: String
     var notes: String?
     let date: Date
     let duration: TimeInterval
     
-    static let mockEvent = Self.init(title: "Mock Event", notes: "Some Notes\n2nd line\n3rd line", date: Date(), duration: 30)
+    static let mockEvent = Self.init(id: UUID(), title: "Mock Event", notes: "Some Notes\n2nd line\n3rd line", date: Date(), duration: 30)
 }
